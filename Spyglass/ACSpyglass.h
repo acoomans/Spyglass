5625b462e1442e69847603eddfca41fe51fc1835
Arnaud Coomans <arnaud.coomans@gmail.com>
2013-03-11 10:53:36 -0700

    Added files

---
+++ Spyglass/ACSpyglass.h
@@ -0,0 +1,13 @@
+//
+//  ACSpyglass.h
+//  SpyglassDemo
+//
+//  Created by Arnaud Coomans on 3/11/13.
+//  Copyright (c) 2013 acoomans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACSpyglass : NSObject
+
+@end
---
+++ Spyglass/ACSpyglass.m
@@ -0,0 +1,13 @@
+//
+//  ACSpyglass.m
+//  SpyglassDemo
+//
+//  Created by Arnaud Coomans on 3/11/13.
+//  Copyright (c) 2013 acoomans. All rights reserved.
+//
+
+#import "ACSpyglass.h"
+
+@implementation ACSpyglass
+
+@end
